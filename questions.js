28e8d88e2eabac342db205b1db341440c8cd8b32
Syd Rigel <prest261@umn.edu>
2019-11-11 18:07:22 -0700

    added all the quiz questions to the questions.js file

---
+++ questions.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "The first item/element in an array of 4 is at index ____.",
+        choices: ["1", "0", "2", "4"],
+        answer: "0"
+    },
+    {
+        title: "How do you select an html element to use in javascript?",
+        choices: ["setAttribute", "querySelector", "appendChild", "innerHTML"],
+        answer: "querySelector"
+    },
+    {
+        title: "The formatting language used to create Web Pages is called:",
+        choices: ["HTML", "Perl", "Java", "CSS"],
+        answer: "HTML"
+    },
+    {
+        title: "____ is the process of finding errors and fixing them within a program.",
+        choices: ["Compiling", "Executing", "Scanning", "Debugging"],
+        answer: "Debugging"
+    }
+  ];
+  
