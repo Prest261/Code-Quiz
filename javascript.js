8ddae5b79511e6d848a0328ff424bfd9f609a033
Syd Rigel <prest261@umn.edu>
2019-11-17 18:23:07 -0700

    created quiz to begin on click, and for the questions to run through the array, as well as the timer running

--- javascript.js
+++ javascript.js
@@ -8,16 +8,127 @@
 // When questions are answered/or when time runs out the user is presented with their final score & give initials
 // final score & initials are then stored in localStorage
 
-// 1) when start button is clicked first question begins (addeventlistener) 
+console.log(questions);
+
+var areagame = document.querySelector("#game");
+var showTimer = document.querySelector("#Timer");
+var showHighScore = document.querySelector("#High-Score");
+var startButton = document.querySelector("#startButton");
+var inputArea = document.querySelector("#input");
+var nameArea = document.querySelector("#nameButton");
+var secondsLeft = 90;
+var currentQ = 0;
+var timer;
+var score = 0;
+
+// 1) when start button is clicked first question begins (addeventlistener)
+startButton.addEventListener("click", quizQuestions);
+
 // & timer starts countdown (90 seconds)
 
+function Countdown() {
+  secondsLeft--;
+  console.log("secondsLeft", secondsLeft);
+  // show  secondsleft on the DOM
+  showTimer.textContent = secondsLeft;
+  console.log(showTimer);
+
+  if (secondsLeft < 1) {
+    // then end quiz
+    gameEnd();
+  }
+}
+
+// function to loop through the array of questions
+
+function quizQuestions() {
+  console.log("start");
+  timer = setInterval(Countdown, 1000);
+
+  nextQuestion();
+}
+
+function nextQuestion() {
+  console.log("next question", currentQ, questions.length);
+
+  areagame.innerHTML = "";
+  if (currentQ >= questions.length) {
+    console.log("game end");
+    gameEnd();
+  } else {
+    console.log("ELSE");
+    var questionEl = document.createElement("p");
+    questionEl.textContent = questions[currentQ].title;
+    areagame.appendChild(questionEl);
+
+    for (var i = 0; i < questions[currentQ].choices.length; i++) {
+      console.log("i", i);
+      var choice = document.createElement("button");
+      choice.textContent = questions[currentQ].choices[i];
+      choice.setAttribute("class", "choices");
+      choice.setAttribute("value", questions[currentQ].choices[i]);
+      choice.onclick = verify;
+      areagame.appendChild(choice);
+    }
+
+    // show question  questions[currentQ]
+    // show responses
+    // onclick to the responses
+  }
+}
+function verify() {
+  console.log(this);
+  console.log(this.value);
+
+  var answer = questions[currentQ].answer;
+  console.log(answer);
+  if (this.value === answer) {
+    this.setAttribute("style", "color:white; background-color:green");
+  } else {
+    this.setAttribute("style", "color:white; background-color:red");
+    secondsLeft = secondsLeft - 15;
+    // show the seconds
+  }
+
+  currentQ++;
+  setTimeout(nextQuestion, 1000);
+
+  // if correct
+  // show color
+  // next question
+  // incorrect
+  // show color
+  // reduce seconsleft
+  // next question
+}
+function gameEnd() {
+  // clear the timer
+  clearInterval(timer);
+  // show the results
+  // ask for name
+  var input = document.createElement("input");
+    input.textContent = "";
+    inputArea.appendChild(input);
+  var nameBtn = document.createElement("button");
+    nameBtn.textContent = "Submit Name";
+    nameArea.appendChild(nameBtn);
+  nameBtn.addEventListener("click", function() {
+    // save in local storage
+    localStorage.getItem("name", inputArea);
+    console.log(inputArea)
+    localStorage.getItem("secondsleft", secondsLeft);
+    console.log(secondsLeft);
+
+  })
+    
+}
+
+// function to show the user if their answer is correct or not
 
 // 2) after first question is completed display correct/incorrect, next question in the array is presented
 
-
 // 3) if question is correct then move to next question and add 5 points to score
 //   if incorrect then deduct -15 seconds from time & move on to next question
-
 
 // 4) run through all 6 questions, if time is left then add remaining seconds to score.
 //    if time runs out then stop quiz and display final score
